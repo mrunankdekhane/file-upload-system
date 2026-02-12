4fbb63b479de3381734bdb929a40d46115d1100c
Mrunank Dekhane <mrunankdekhane@gmail.com>
2026-02-12 16:11:45 +0530

    added schema

---
+++ models/File.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema({
+  filename: String,
+  originalname: String,
+  mimetype: String,
+  size: Number,
+  path: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("File", fileSchema);
